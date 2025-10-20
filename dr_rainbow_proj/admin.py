18cb22e525d953b15eaf77777ceb9968c5735a36
Natalia-sys777 <nataliakricak53@gmail.com>
2025-10-20 21:55:05 +0200

    Додано шаблон для списку уроків геометрії

--- dr_rainbow_proj/admin.py
+++ dr_rainbow_proj/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 admin.site.site_header = "Dr Rainbow — Адмінка"
 admin.site.site_title = "Dr Rainbow"
 admin.site.index_title = "Панель адміністратора"
+
--- geometry/admin.py
+++ geometry/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import GeometryLesson  # імпортуємо модель
 
-# Register your models here.
+@admin.register(GeometryLesson)
+class GeometryLessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "level", "duration", "created_at")
+    search_fields = ("title", "summary", "materials")
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("level", "age_range")
+
